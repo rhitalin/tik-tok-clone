c7e80a3098b0834c6963a94ed3fe634da614fe20
Rita <44211750+rhitalin@users.noreply.github.com>
2022-07-01 19:22:26 -0400

    Add files via upload

---
+++ ROOT/src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-analytics.js";
+import firebase from "./firebase";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDIxir-6FTFIlYqtNoCXJWbXV8R9IDUsWE",
+    authDomain: "tik-tok-clone-78cc5.firebaseapp.com",
+    projectId: "tik-tok-clone-78cc5",
+    storageBucket: "tik-tok-clone-78cc5.appspot.com",
+    messagingSenderId: "375608618913",
+    appId: "1:375608618913:web:975ac1533655dcc38fe5e0",
+    measurementId: "G-ZD4HLR3X2V"
+  };
